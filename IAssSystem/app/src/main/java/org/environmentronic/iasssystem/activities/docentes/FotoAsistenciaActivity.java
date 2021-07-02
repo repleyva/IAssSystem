ede5ef2fc45fc3fd3f52c80265dbd52a37a7baea
RusvelLeyva <71774981+RusvelLeyva@users.noreply.github.com>
2021-07-02 10:17:04 -0500

    subiendo foto de asistencia

--- IAssSystem/app/src/main/java/org/environmentronic/iasssystem/activities/docentes/FotoAsistenciaActivity.java
+++ IAssSystem/app/src/main/java/org/environmentronic/iasssystem/activities/docentes/FotoAsistenciaActivity.java
@@ -2,6 +2,7 @@ package org.environmentronic.iasssystem.activities.docentes;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
@@ -9,11 +10,16 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.ExifInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.WindowManager;
@@ -35,6 +41,10 @@ import org.environmentronic.iasssystem.R;
 import org.environmentronic.iasssystem.activities.principales.MainActivity;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class FotoAsistenciaActivity extends AppCompatActivity {
 
@@ -61,6 +71,9 @@ public class FotoAsistenciaActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private Bitmap imageBitmap;
     private StorageReference storageReference;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    private String currentPhotoPath = null;
+    private Bitmap rotatedBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,27 +113,100 @@ public class FotoAsistenciaActivity extends AppCompatActivity {
     }
 
     public void tomarFotoD(View view) {
-        abrirCamara();
+        dispatchTakePictureIntent();
     }
 
-    public void abrirCamara() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    // captura la foto
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "org.environmentronic.iasssystem.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
+    }
+
+    // crea una imagen
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String imageFileName = nombreUsuario;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".png",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
+            imageBitmap = BitmapFactory.decodeFile(currentPhotoPath);
+            rotatedBitmap = rotarImagen(imageBitmap);
             photoAsistencia.setImageBitmap(imageBitmap);
         }
     }
 
-    public void subirFotoFirebaseD(View view) {
+    public Bitmap rotarImagen(Bitmap imageBitmap) {
+        ExifInterface ei = null;
+        try {
+            ei = new ExifInterface(currentPhotoPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED);
+
+        Bitmap rotatedBitmap = null;
+        switch (orientation) {
+
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                rotatedBitmap = rotateImage(imageBitmap, 90);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                rotatedBitmap = rotateImage(imageBitmap, 180);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                rotatedBitmap = rotateImage(imageBitmap, 270);
+                break;
+
+            case ExifInterface.ORIENTATION_NORMAL:
+            default:
+                rotatedBitmap = imageBitmap;
+        }
+        return rotatedBitmap;
+    }
+
+    // la foto rota, entonces
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
+    public void subirFotoFirebaseD(View view) throws IOException {
 
         boolean coneccion = compruebaConexion(this);
 
@@ -128,14 +214,22 @@ public class FotoAsistenciaActivity extends AppCompatActivity {
             if (coneccion) {
                 showProgressBar("Subiendo foto de asistencia, espere ...");
                 // Get the data from an ImageView as bytes
-                photoAsistencia.setDrawingCacheEnabled(true);
-                photoAsistencia.buildDrawingCache();
-                Bitmap bitmap = ((BitmapDrawable) photoAsistencia.getDrawable()).getBitmap();
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-                byte[] data = baos.toByteArray();
+                Uri file = Uri.fromFile(new File(currentPhotoPath));
+                InputStream imageStream = null;
+                try {
+                    imageStream = getApplication().getContentResolver().openInputStream(file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Bitmap original = BitmapFactory.decodeStream(imageStream);
+                //Bitmap bmp = getResizedBitmap(original, 500);
+                Bitmap rotateBitmap = rotateImageIfRequired(getApplicationContext(), original, file);
+                Bitmap bmp = getResizedBitmap(rotateBitmap, 1000);
 
-                UploadTask uploadTask = storageReference.child("DOCENTES/" + idUsuario + "/" + idUsuario + "_" + nombreUsuario + "_" + materia + "." + codigo + "/" + "FOTO_CLASE/" + "asistencia.png").putBytes(data);
+                String path = MediaStore.Images.Media.insertImage(getApplication().getContentResolver(), bmp, String.valueOf(System.currentTimeMillis()), null);
+                Uri imagen = Uri.parse(path);
+
+                UploadTask uploadTask = storageReference.child("DOCENTES/" + idUsuario + "/" + idUsuario + "_" + nombreUsuario + "_" + materia + "." + codigo + "/" + "FOTO_CLASE/" + "asistencia.png").putFile(imagen);
                 uploadTask.addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception exception) {
@@ -160,6 +254,48 @@ public class FotoAsistenciaActivity extends AppCompatActivity {
             Toast.makeText(this, "Debe tomar una foto", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public static Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+
+        return Bitmap.createScaledBitmap(image, width, height, true);
+
+    }
+
+    private static Bitmap rotateImageIfRequired(Context context, Bitmap img, Uri selectedImage) throws IOException {
+
+        InputStream input = context.getContentResolver().openInputStream(selectedImage);
+        ExifInterface ei;
+        if (Build.VERSION.SDK_INT > 23)
+            ei = new ExifInterface(input);
+        else
+            ei = new ExifInterface(selectedImage.getPath());
+
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotateImage(img, 90);
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotateImage(img, 180);
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotateImage(img, 270);
+            default:
+                return img;
+        }
+    }
+
 
     public void setBtnBorrarFotoD(View view) {
 
